811ba3e4220612fdeeed40e738f016231e48c395
Ryan S. Brown <sb@ryansb.com>
2016-01-27 16:38:35 -0500

    Add consumer that actually scrapes pages & finds links

---
+++ consumer.py
@@ -0,0 +1,51 @@
+# 1. Claim a message
+# 2. Grab HTML
+# 3. Delete message
+# 4. Push more URLs
+from bs4 import BeautifulSoup
+import sys
+from helpers import client
+import requests
+import urlparse
+
+def scrape_generator(url):
+    parent = urlparse.urlsplit(url)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    for tag in soup.findAll('a'):
+        link = tag.get('href')
+
+        if '#' in link:
+            link = ''.join(link.split('#')[:-1])
+
+        if link.startswith('mailto:'):
+            continue
+        elif urlparse.urlsplit(link).scheme in ('http', 'https'):
+            yield link
+        elif link.startswith('/'):
+            yield parent.scheme + '://' + parent.netloc + link
+        elif parent.path.endswith('/'):
+            yield parent.scheme + '://' + parent.netloc + parent.path + link
+        else:
+            yield parent.scheme + '://' + parent.netloc + parent.path + '/' + link
+
+if __name__ == '__main__':
+    #for u in scrape('http://rsb.io/'):
+    #    print u
+
+    scrape = client.queue('scrape')
+    ingest = client.queue('ingest')
+    complete = client.queue('completed')
+
+    while True:
+        claimed = scrape.claim(ttl=180, grace=60, limit=1)
+        for msg in claimed:
+            messages = [
+                {'body': u, 'ttl': 180}
+                for u in scrape_generator(msg.body)
+                if u.startswith(sys.argv[1])
+            ]
+            if len(messages):
+                ingest.post(messages)
+            complete.post({'body': msg.body, 'ttl': 300})
+            msg.delete()
